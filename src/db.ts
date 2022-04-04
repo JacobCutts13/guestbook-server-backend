3c7de4dc5bee293efd36a4b47c73177a93c713f3
Jc <Jc@email.com>
2022-04-04 12:32:41 +0100

    added data

--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,6 @@
 export interface DbItem {
-  // sketch out interface here
+  name: string,
+  message: string
 }
 
 export interface DbItemWithId extends DbItem {
@@ -21,7 +22,8 @@ export const addDummyDbItems = (n: number): DbItemWithId[] => {
   const createdSignatures: DbItemWithId[] = [];
   for (let count = 0; count < n; count++) {
     const createdSignature = addDbItem({
-      // possibly add some generated data here
+      name: "jacob",
+      message: "msg", 
     });
     createdSignatures.push(createdSignature);
   }
